b91e1fbf1ca5d740a2f9a7cb07d1941e21e068cd
Hubert Wójtowicz <hubert.karol.wojtowicz@gmail.com>
2020-07-02 20:56:43 +0000

    add: helper script to install jenkins

--- scripts/install_jenkins.sh
+++ scripts/install_jenkins.sh
@@ -3,6 +3,10 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt install default-jdk -y
 
+#issue with pub GPG jenkins key 
+#https://knowledge.udacity.com/questions/139443
+#https://askubuntu.com/questions/660599/i-am-installing-jenkins-server-but-its-giving-w-gpg-error
+
 # add the repo key to the system
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 
@@ -16,4 +20,12 @@ sudo apt-get install jenkins -y
 
 sudo systemctl status jenkins
 
-# all following steps must be done manually
+# optional 
+    # Start the Jenkins server
+    sudo systemctl start jenkins
+    
+    # Enable the service to load during boot
+    sudo systemctl enable jenkins
+# all following steps must be done manually, like unlocking jenkins, installing packages etc.
+
+sudo vim /var/lib/jenkins/secrets/initialAdminPassword #copy initial jenkins password
