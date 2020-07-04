bb7272cad255792e98ec9a56892d370b50f8eedd
Hubert Wójtowicz <hubert.karol.wojtowicz@gmail.com>
2020-07-04 07:27:07 +0000

    aws cli and eks tools

---
+++ public/valuable-links.md
@@ -0,0 +1,3 @@
+[docker installation at ubuntu 18.04](https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-18-04)
+[eksctl configuration](https://docs.aws.amazon.com/eks/latest/userguide/getting-started-eksctl.html)
+[update eksctl on linux](https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2-linux.html#cliv2-linux-upgrade)
---
+++ scripts/ubuntu_prepare_for_eks_connection.sh
@@ -0,0 +1,31 @@
+#!bin/bash
+
+# based on https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2-linux.html#cliv2-linux-upgrade
+
+cd ~
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+ls -l /usr/local/bin/aws
+which aws
+sudo ./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update
+aws --version
+
+
+# create jenkis IAM role acc https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-quickstart.html#cli-configure-quickstart-creds
+aws configure # assign IAM values
+aws configure list
+
+# install eksctl
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+eksctl version
+
+# install kubectl
+curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.16.8/2020-04-16/bin/linux/amd64/kubectl
+
+curl -o kubectl.sha256 https://amazon-eks.s3.us-west-2.amazonaws.com/1.16.8/2020-04-16/bin/linux/amd64/kubectl.sha256
+openssl sha1 -sha256 kubectl
+
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin
